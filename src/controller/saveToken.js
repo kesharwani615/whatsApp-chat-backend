4f40e6f610495ec8e093772f446d69c34a54411e
shivam kesharwani <shivam.kesharwani@mobulous.com>
2026-05-15 18:04:41 +0530

    feat: add controller to handle FCM token registration and storage

--- src/controller/saveToken.js
+++ src/controller/saveToken.js
@@ -1,31 +1,31 @@
 import express from 'express';
-import FcmToken from '../models/FcmToken.js';
+import FCMToken from '../models/FCMToken.js';
 const router = express.Router();
 
 router.post("/save-token", async (req, res) => {
-    try {
-      const { token } = req.body;
-  
-      if (!token) {
-        return res.status(400).json({ success: false, message: "Token missing" });
-      }
-  
-      // Check if token already exists
-      const existing = await FcmToken.findOne({ token });
-      if (!existing) {
-        await FcmToken.create({ token });
-      }
-  
-      return res.json({
-        success: true,
-        message: "Token saved successfully",
-      });
-    } catch (err) {
-      console.error("Error saving token:", err);
-      return res
-        .status(500)
-        .json({ success: false, message: "Server error", error: err });
+  try {
+    const { token } = req.body;
+
+    if (!token) {
+      return res.status(400).json({ success: false, message: "Token missing" });
     }
-  });
-  
-  export default router;
+
+    // Check if token already exists
+    const existing = await FcmToken.findOne({ token });
+    if (!existing) {
+      await FCMToken.create({ token });
+    }
+
+    return res.json({
+      success: true,
+      message: "Token saved successfully",
+    });
+  } catch (err) {
+    console.error("Error saving token:", err);
+    return res
+      .status(500)
+      .json({ success: false, message: "Server error", error: err });
+  }
+});
+
+export default router;
